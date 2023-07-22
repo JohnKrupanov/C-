7ed35829997a8ea66fca4ef5b8fea1cd1f8caa48
john <john79@yandex.ru>
2023-07-22 18:46:25 +0500

    Промежуточное сохранение

---
+++ seminar1/program.cs
@@ -0,0 +1,21 @@
+﻿/*
+int intNumber = 25;
+double doubleNumber = 12.12;
+string text = "some text";
+bool logicVar = trug; 
+*/
+
+/*
+int num1 = 5;
+int num2 = 10;
+Console.WriteLine("мой номер " + num1 + " and " + num2 + " and it is good!");
+Console.WriteLine($"мой номер {num1} and {num2} and it is good!");
+*/
+
+/*
+Console.Write("input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Your number is " + num);
+*/
+
