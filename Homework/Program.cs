914bec122b409168cf8877de72d46648b6fdd0df
john <john79@yandex.ru>
2023-08-01 23:42:26 +0500

    записаны задачи 25, 27 и 29

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -212,15 +212,67 @@ cube(num);
 */
 
 
-// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Задача 25: 
+// Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+/*
+Console.WriteLine("введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int degree = a;
+
+for (int i = 1; i < b; i++)
+{
+degree = degree * a;
+}
+Console.WriteLine("A в степени B равно: " + degree);
+*/
+
+// Задача 27: 
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
 
-// Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+/*
+int SummaNumDigRemOfDiv(int num)
+{
+    int res = 0;
+    while (num >0)
+    {
+        res = res + num%10;
+        num = num /10;
+    }
+    return res;
+}
+Console.WriteLine("Введите любое натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SummaNumDigRemOfDiv(num));
+*/
+
+// Задача 29: 
+// Напишите программу, которая задаёт массив из m элементов и 
+// выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+/*
+int lenArray = ReadInt("Введите длинну массива: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,34);
+    Console.Write(randomArray[i] + " ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+*/
