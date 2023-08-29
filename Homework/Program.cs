bcad735e2f9ce29e4fb2b60979eaf27427d38f0b
john <john79@yandex.ru>
2023-08-29 23:03:54 +0500

    Task 47

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -483,3 +483,53 @@ double y = yVolume(b1, k1, x);
 Console.WriteLine("Значение Х = " + x);
 Console.WriteLine("Значение Y = " + y);
 */
+
+
+//  Task 47
+//  Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+/*
+double[,] CreateRandom2dArray()
+{
+    Console.Write("Input a rows of array: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Input a columns of array: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+
+    int i = 0; int j = 0;
+
+    double[,] array = new double[rows, columns];
+    while (i < rows)
+    {
+        j = 0;
+        while (j < columns)
+        {
+            array[i, j] = Math.Round(new Random().Next(-10, 11) + new Random().NextDouble(), 1);
+            j++;
+        }
+        i++;
+    }
+    return array;
+}
+
+void Print2dArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] myArray = CreateRandom2dArray();
+Print2dArray(myArray);
+*/
