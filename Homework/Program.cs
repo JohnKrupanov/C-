c137e6286a8e064393b91653f3a5d73f0f3de270
john <john79@yandex.ru>
2023-08-30 22:28:51 +0500

    Task 47, 50, 52

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -533,3 +533,179 @@ void Print2dArray(double[,] array)
 double[,] myArray = CreateRandom2dArray();
 Print2dArray(myArray);
 */
+
+// Task 50
+
+// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+/*
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int[,] Gen2DArrFibo(int column, int row)
+{
+    int[] arr1D = new int[column * row];
+    arr1D[0] = 0;
+    arr1D[1] = 1;
+    for (int i = 2; i < arr1D.Length; i++)
+    {
+        arr1D[i] = arr1D[i - 1] + arr1D[i - 2];
+    }
+    int[,] arr2D = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr2D[i, j] = arr1D[i * column + j];
+        }
+    }
+    return arr2D;
+}
+
+void SerchElemAndPrint(int[,] arr2D, int serchElem)
+{
+    bool elemSerch = false;
+    for (int i = 0; i < arr2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr2D.GetLength(1); j++)
+        {
+            if (serchElem == arr2D[i, j])
+            {
+                elemSerch = true;
+                Console.ForegroundColor = System.ConsoleColor.Red;
+            }
+            Console.Write(arr2D[i, j] + " \t");
+            Console.ForegroundColor = System.ConsoleColor.White;
+        }
+        Console.WriteLine();
+    }
+    if (elemSerch == false)
+    {
+        Console.ForegroundColor = System.ConsoleColor.Red;
+        Console.WriteLine("Элемент " + serchElem + " не найден");
+    }
+    Console.ForegroundColor = System.ConsoleColor.White;
+}
+
+int column = ReadData("Количество столбцов матрицы: ");
+int row = ReadData("Количество строк матрицы: ");
+int findeNum = ReadData("Введите искомое число ");
+
+int[,] arr2DFibo = Gen2DArrFibo(column, row);
+
+Console.WriteLine();
+SerchElemAndPrint(arr2DFibo, findeNum);
+*/
+
+// Task 52
+// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+/*
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int[,] Gen2DArrayInt(int row, int column)
+{
+    int i = 0; int j = 0;
+    int[,] arr = new int[row, column];
+    while (i < row)
+    {
+        j = 0;
+        while (j < column)
+        {
+            arr[i, j] = new Random().Next(0, 101);
+            j++;
+        }
+        i++;
+    }
+    return arr;
+}
+
+void Print2DArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "\t");
+        }
+        Console.WriteLine("");
+    }
+}
+
+void Print1DArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+int[] MeanColum(int[,] arr)
+{
+    int[] means = new int[arr.GetLength(1)];
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        for (int j = 0; j < arr.GetLength(0); j++)
+        {
+            means[i] = means[i] + arr[j, i];
+        }
+        means[i] = means[i] / arr.GetLength(0);
+    }
+    return means;
+}
+
+int DiagAverage(int[,] arr)
+{
+    int res = 0;
+    int count = 0;
+    int min = arr.GetLength(0) > arr.GetLength(1) ? arr.GetLength(1) : arr.GetLength(0);
+
+    for (int i = 0; i < min; i = i + 1)
+    {
+        res = res + arr[i, i];
+        count = count + 1;
+    }
+    res = res / count;
+    return res;
+}
+
+int row = ReadData("Количество строк матрицы: ");
+int column = ReadData("Количество столбцов матрицы: ");
+
+int[,] arr2D = Gen2DArrayInt(row, column);
+
+int[] average = MeanColum(arr2D);
+
+Print2DArr(arr2D);
+
+Console.WriteLine();
+Console.WriteLine("Среднее арифметическое по столбцам: ");
+
+Print1DArr(average);
+
+int diagAver = DiagAverage(arr2D);
+Console.WriteLine("Среднее арифметическое по диагонали: ");
+Console.WriteLine(diagAver);
+*/
